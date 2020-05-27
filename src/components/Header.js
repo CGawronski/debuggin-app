8a8bb55dd417b2ebca8bad375e1f4b286d1fade2
Colin Gawronski <colingawronski@gmail.com>
2020-05-27 11:35:03 -0400

    added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">
+          <b>DEBUGGIN'</b>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <NavDropdown title="New Ticket" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">New Bug</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                New Proposal
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                New Question
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={NavLink} to="/">
+              All Tickets
+            </Nav.Link>
+            <Nav.Link eventKey={2}>Sign In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
 }
