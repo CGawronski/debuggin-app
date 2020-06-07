33a8b8e6fd0eb8aff20a46eaecfebf532cdc1bf1
Colin Gawronski <colingawronski@gmail.com>
2020-06-07 15:41:20 -0400

    commit

--- src/tickets.js
+++ src/tickets.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://debuggin-app-clg.herokuapp.com",
 });
 
-// "https://debuggin-app-clg.herokuapp.com"
+// "http://localhost:4000"
